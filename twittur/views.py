7fd282303e9eac0b378d9966ef799fe60b69f140
syafiq <syafiq@dot.local>
2018-01-26 17:02:51 +0800

    render search result on template

--- twittur/views.py
+++ twittur/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -15,6 +17,11 @@ def search_keyword(request):
             keyword = keyword_search_form.cleaned_data['keyword']
             count = keyword_search_form.cleaned_data['count']
             results = tweep.keyword_search(request, keyword, count)
+
+            json_list = []
+            for x in results:
+                json_list.append(json.dumps(x._json))
+
             return render(request, 'twittur/show.html', {'result':results})
 
     return render(
