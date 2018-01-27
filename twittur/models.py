f20d0504ef728dc39f63cad9a8b0ff21c5176246
unknown <ahmadsyafiq93@gmail.com>
2018-01-28 04:19:31 +0800

    twittur migrations

--- twittur/models.py
+++ twittur/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class SearchItem(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    keyword = models.CharField(max_length=200, blank=False)
+    is_user_search = models.BooleanField()
+    searched_at = models.DateTimeField(auto_now_add=True)
+
+class Tweets(models.Model):
+    search_word = models.ForeignKey(SearchItem, on_delete=models.CASCADE)
+    data = JSONField()
