22122056c3469a78b67ca19f727c7f8bf4c7724e
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2018-02-12 01:48:45 +0800

    charts

--- sental/views.py
+++ sental/views.py
@@ -3,6 +3,7 @@ from twittur.models import SearchItem, Tweets
 from twittur.models import Tweets
 
 from . import classify
+from .fusioncharts import FusionCharts
 
 def index(request):
     saved_searched = SearchItem.objects.filter(user=request.user.id)
@@ -28,4 +29,42 @@ def analyse(request, search_id):
         'pos': pos,
         'neut': neut,
     }
-    return render(request, 'sental/analyse.html', context=context)
+
+    pie_chart = {}
+    pie_chart['chart'] = {
+        "caption": "Analysis Result",
+        "subCaption": " ",
+        "numberPrefix": "$",
+        "showBorder": "0",
+        "use3DLighting": "0",
+        "enableSmartLabels": "0",
+        "startingAngle": "310",
+        "showLabels": "0",
+        "showPercentValues": "1",
+        "showLegend": "1",
+        "defaultCenterLabel": "Total revenue: $64.08K",
+        "centerLabel": "Revenue from $label: $value",
+        "centerLabelBold": "1",
+        "showTooltip": "0",
+        "decimals": "0",
+        "useDataPlotColorForLabels": "1",
+        "theme": "zune"
+    }
+    pie_chart['data'] = [
+        {
+            "label": "Negative Tweets",
+            "value": neg
+        },
+        {
+            "label": "Positive Tweets",
+            "value": pos
+        },
+        {
+            "label": "Neutral Tweets",
+            "value": neut
+        },
+    ]
+
+    pie = FusionCharts('pie2d', 'pie', '600', '400', 'chart', 'json', pie_chart)
+ 
+    return render(request, 'sental/analyse.html', {'output': pie.render()})
